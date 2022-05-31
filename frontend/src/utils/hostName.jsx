a3b787d8e6cdcef26484242b22060f967ab4c896
Biraj Khanal <biraj@pop-os.localdomain>
2022-05-31 21:15:28 +0545

    update

--- frontend/src/utils/hostName.jsx
+++ frontend/src/utils/hostName.jsx
@@ -1,2 +1,2 @@
-// export const host = "https://backend-ceonc.herokuapp.com"
-export const host = "http://localhost:4000"
+export const host = "https://backend-ceonc.herokuapp.com"
+// export const host = "http://localhost:4000"
